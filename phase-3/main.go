6929404ae714534b91cabb836980c7a85ebcd934
deelawn <dboltz03@gmail.com>
2024-05-22 06:20:11 +0200

    fixes

--- phase-3/main.go
+++ phase-3/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gnolang/gno/gno.me/apps"
 	"github.com/gnolang/gno/gno.me/gno"
 	"github.com/gnolang/gno/gno.me/http"
 )
 
+const port = "4591"
+
 func main() {
-	vm, _ := gno.NewVM()
-	apps.CreateInstaller(vm)
-	server := http.NewServer(vm)
+	vm, firstStartup := gno.NewVM()
+	if firstStartup {
+		if err := apps.CreatePort(vm); err != nil { // installer dependency
+			fmt.Println("could not create port app: " + err.Error())
+			return
+		}
+
+		if err := apps.CreateInstaller(vm); err != nil {
+			fmt.Println("could not create installer app: " + err.Error())
+			return
+		}
+	}
+
+	server := http.NewServer(vm, port)
 	if err := server.ListenAndServe(); err != nil {
 		panic("could not start server: " + err.Error())
 	}
